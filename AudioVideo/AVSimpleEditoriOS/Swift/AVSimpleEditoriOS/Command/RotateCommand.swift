6e9f131ac2feca11f48aef32a5f98a862c0f0400
bqlin <bqlins@163.com>
2021-08-18 16:52:58 +0800

    [AVSimpleEditoriOS] 简化旋转逻辑

--- AudioVideo/AVSimpleEditoriOS/Swift/AVSimpleEditoriOS/Command/RotateCommand.swift
+++ AudioVideo/AVSimpleEditoriOS/Swift/AVSimpleEditoriOS/Command/RotateCommand.swift
@@ -16,45 +16,41 @@ class RotateCommand: Command {
         guard let composition = composition else { return }
         guard let videoTrack = composition.tracks(withMediaType: .video).first else { return }
         
-        var instruction: AVMutableVideoCompositionInstruction
-        var layerInstruction: AVMutableVideoCompositionLayerInstruction
+        let instruction: AVMutableVideoCompositionInstruction
+        let layerInstruction: AVMutableVideoCompositionLayerInstruction
         
         // 每次顺时针旋转90度
+        let naturalSize, renderSize: CGSize
+        var t: CGAffineTransform = .identity
         if let videoComposition = videoComposition {
-            let size = videoComposition.renderSize
-            let renderSize = size.swap
-            
-            // Translate the composition to compensate the movement caused by rotation (since rotation would cause it to move out of frame)
-            // Rotate transformation
-            let t = CGAffineTransform(translationX: size.height, y: 0).rotated(by: degreesToRadians(90))
-            
-            videoComposition.renderSize = renderSize
-            
+            naturalSize = videoComposition.renderSize
             instruction = videoComposition.instructions.first! as! AVMutableVideoCompositionInstruction
             layerInstruction = instruction.layerInstructions.first! as! AVMutableVideoCompositionLayerInstruction
             
             var exitingTransform = CGAffineTransform.identity
             _ = layerInstruction.getTransformRamp(for: composition.duration, start: &exitingTransform, end: nil, timeRange: nil)
-            layerInstruction.setTransform(exitingTransform.concatenating(t), at: .zero)
+            t = exitingTransform.concatenating(t)
         } else {
-            let size = videoTrack.naturalSize
-            let renderSize = size.swap
-            
-            // Translate the composition to compensate the movement caused by rotation (since rotation would cause it to move out of frame)
-            // Rotate transformation
-            let t = CGAffineTransform(translationX: size.height, y: 0).rotated(by: degreesToRadians(90))
-            
+            naturalSize = videoTrack.naturalSize
             let videoComposition = AVMutableVideoComposition()
-            videoComposition.renderSize = renderSize
             videoComposition.frameDuration = CMTime(value: 1, timescale: 30)
             
             instruction = AVMutableVideoCompositionInstruction()
             instruction.timeRange = CMTimeRange(start: .zero, end: composition.duration)
             layerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: videoTrack)
-            layerInstruction.setTransform(t, at: .zero)
-            
             self.videoComposition = videoComposition
         }
+        renderSize = naturalSize.swap
+        videoComposition!.renderSize = renderSize
+        
+        // Translate the composition to compensate the movement caused by rotation (since rotation would cause it to move out of frame)
+        // Rotate transformation
+        // 由于矩阵不满足结合律，所以不能写成下面
+        // t = t.translatedBy(x: naturalSize.height, y: 0).rotated(by: degreesToRadians(90))
+        // 需写成这样：
+        let transform = CGAffineTransform.identity.translatedBy(x: naturalSize.height, y: 0).rotated(by: degreesToRadians(90))
+        t = t.concatenating(transform)
+        layerInstruction.setTransform(t, at: .zero)
         
         instruction.layerInstructions = [layerInstruction]
         videoComposition!.instructions = [instruction]
